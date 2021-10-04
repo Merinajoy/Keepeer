75198414d371e9f7c6d1d2353dc73d3350269c23
Merina Joy <you@example.com>
2021-10-04 15:07:07 +0530

    Keeper initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+    const year = new Date().getFullYear();
+    return (<div className="footer">
+      <p>Copyright © {year}</p>
+    </div>)
+}
+
+export default Footer;
